ee8b3f088eee12d207ce6b882ce6d06e30802a51
flamur7 <flamur.ga7@gmail.com>
2022-01-23 16:31:55 +0100

    Add project files.

---
+++ UniversityManagementSystem/Models/Branch.cs
@@ -0,0 +1,9 @@
+﻿namespace UniversityManagementSystem.Models
+{
+    public class Branch
+    {
+        public int BranchId { get; set; }
+        public string BranchName { get; set; }
+
+    }
+}
